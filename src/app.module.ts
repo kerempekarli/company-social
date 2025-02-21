12bb42684f007e3a61f1d5bf31dcdb74a2de8709
kerempekarli <kerempekarli@gmail.com>
2025-02-22 06:28:18 +0700

    AppModule updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,14 @@ import { AuthController } from './modules/auth/auth.controller';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { DepartmentModule } from './modules/department/department.module';
+import { TeamModule } from './modules/team/team.module';
+import { TeamController } from './modules/team/team.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
-  TypeOrmModule.forRoot(ormconfig), UserModule, AuthModule, DepartmentModule
+  TypeOrmModule.forRoot(ormconfig), UserModule, AuthModule, DepartmentModule, TeamModule
   ],
-  controllers: [AppController, UserController, AuthController],
+  controllers: [AppController, UserController, AuthController, TeamController],
   providers: [AppService],
 })
 export class AppModule { }
--- src/modules/team/team.module.ts
+++ src/modules/team/team.module.ts
@@ -4,9 +4,11 @@ import { Team } from './entities/team.entity';
 import { UserTeam } from './entities/user_team.entity';
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
+import { User } from '../user/entities/user.entity';
+import { Department } from '../department/entities/department.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Team, UserTeam])],
+    imports: [TypeOrmModule.forFeature([Team, UserTeam, User, Department])],
     controllers: [TeamController],
     providers: [TeamService],
     exports: [TeamService],
