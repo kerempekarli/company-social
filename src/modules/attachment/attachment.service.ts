10b4f67ae43a2b8c1790b4501bb345de7368f44c
kerempekarli <kerempekarli@gmail.com>
2025-02-26 05:15:54 +0700

    Attachment Static files support added

--- src/modules/attachment/attachment.service.ts
+++ src/modules/attachment/attachment.service.ts
@@ -3,13 +3,18 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Attachment } from './entities/attachment.entity';
+import { Post } from 'src/modules/post/entities/post.entity';
+import { Comment } from 'src/modules/comment/entities/comment.entity';
 
 @Injectable()
 export class AttachmentService {
   constructor(
     @InjectRepository(Attachment)
     private readonly attachRepo: Repository<Attachment>,
-  ) {}
+    // If you need to fetch post/comment to verify, you might inject them as well:
+    // @InjectRepository(Post) private readonly postRepo: Repository<Post>,
+    // @InjectRepository(Comment) private readonly commentRepo: Repository<Comment>,
+  ) { }
 
   findAll(): Promise<Attachment[]> {
     return this.attachRepo.find({ relations: ['post', 'comment'] });
@@ -20,13 +25,16 @@ export class AttachmentService {
       where: { attachmentId: id },
       relations: ['post', 'comment'],
     });
-    if (!attach) throw new NotFoundException(`Attachment #${id} not found`);
+    if (!attach) {
+      throw new NotFoundException(`Attachment #${id} not found`);
+    }
     return attach;
   }
 
-  create(data: Partial<Attachment>): Promise<Attachment> {
-    const attach = this.attachRepo.create(data);
-    return this.attachRepo.save(attach);
+  // This method is called after the file is physically saved by Multer
+  async create(data: Partial<Attachment>): Promise<Attachment> {
+    const attachment = this.attachRepo.create(data);
+    return this.attachRepo.save(attachment);
   }
 
   async remove(id: number): Promise<void> {
